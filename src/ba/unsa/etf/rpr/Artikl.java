922347acea51f3b0c5d6d42f732a507bdd20c87c
abegic2 <abegic2@etf.unsa.ba>
2020-10-26 20:02:25 +0100

    Dodan dio metoda

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -3,6 +3,36 @@ package ba.unsa.etf.rpr;
 public class Artikl {
     private String naziv;
     private double cijena;
-    private int kod;
-    
+    private String kod;
+
+    public Artikl(String naziv, double cijena, String kod) {
+        this.naziv=naziv;
+        this.cijena=cijena;
+        this.kod=kod;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -2,6 +2,15 @@ package ba.unsa.etf.rpr;
 
 public class Supermarket {
     private Artikl [] artikli= new Artikl[1000];
+    private int brojArtikala=0;
 
+    public boolean dodajArtikl(Artikl artikal) {
+        if(brojArtikala<1000) {
+            artikli[brojArtikala]= new Artikl(artikal.getNaziv(),artikal.getCijena(),artikal.getKod());
+            brojArtikala=brojArtikala+1;
+            return true;
+        }
+        return false;
+    }
 
 }
