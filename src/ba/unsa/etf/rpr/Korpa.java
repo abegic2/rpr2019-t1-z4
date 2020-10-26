df065b44a786a886ef2b7c62eede2d7fc1e08b93
abegic2 <abegic2@etf.unsa.ba>
2020-10-26 20:31:44 +0100

    Zavrsen ispravan kod

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -4,7 +4,45 @@ public class Korpa {
     private Artikl [] artikli = new Artikl[50];
     private int brojArtikala=0;
 
+    public boolean dodajArtikl(Artikl artikal) {
+        if(brojArtikala<50) {
+            artikli[brojArtikala]= new Artikl(artikal.getNaziv(),artikal.getCijena(),artikal.getKod());
+            brojArtikala=brojArtikala+1;
+            return true;
+        }
+        return false;
+    }
 
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
 
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i=0; i<brojArtikala; i++) {
+            if(artikli[i].getKod().equals(kod)) { // ako je artikl pronadjen
+                Artikl izbaceniArtikl = new Artikl(artikli[i].getNaziv(),artikli[i].getCijena(),artikli[i].getKod());
+                artikli[i]=null;
+                for(int j=i; j<brojArtikala-1; j++) {
+                    artikli[i]=artikli[i+1];
+                }
+                artikli[brojArtikala-1]=null;
+                brojArtikala=brojArtikala-1;
+                return izbaceniArtikl;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijena=0;
+        for(int i=0; i<brojArtikala; i++) {
+            ukupnaCijena=ukupnaCijena+artikli[i].getCijena();
+        }
+        return ukupnaCijena;
+    }
 
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -4,6 +4,22 @@ public class Supermarket {
     private Artikl [] artikli= new Artikl[1000];
     private int brojArtikala=0;
 
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public int getBrojArtikala() {
+        return brojArtikala;
+    }
+
+    public void setBrojArtikala(int brojArtikala) {
+        this.brojArtikala = brojArtikala;
+    }
+
     public boolean dodajArtikl(Artikl artikal) {
         if(brojArtikala<1000) {
             artikli[brojArtikala]= new Artikl(artikal.getNaziv(),artikal.getCijena(),artikal.getKod());
@@ -12,5 +28,22 @@ public class Supermarket {
         }
         return false;
     }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i=0; i<brojArtikala; i++) {
+            if(artikli[i].getKod().equals(kod)) { // uslov pronalaska
+                Artikl izbaceniArtikl = new Artikl(artikli[i].getNaziv(),artikli[i].getCijena(),artikli[i].getKod());
+                artikli[i]=null;
+                for(int j=i; j<brojArtikala-1; j++) { // izbacivanje
+                    artikli[i]=artikli[i+1];
+                }
+                artikli[brojArtikala-1]=null;
+                brojArtikala=brojArtikala-1;
+                return izbaceniArtikl;
+            }
+        }
+        return null;
+    }
+
 
 }
